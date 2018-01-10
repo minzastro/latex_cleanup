8ca7b894506a84937e737c33bcfdee1a589a7d67
Alexey Mints <mints@mps.mpg.de>
2018-01-10 14:25:42 +0100

    Bugs fixed

--- checks/aa.py
+++ checks/aa.py
@@ -10,7 +10,7 @@ import re
 
 def load_aa_substitutes():
     def turn_to_re(s_from, s_to):
-        re_from = r'(%s)' % re.sub(r'(\.|\(|\))', r'\\\1', s_from)
+        re_from = r'(\b%s)' % re.sub(r'(\.|\(|\))', r'\\\1', s_from)
         re_to = r'{\color{red}\1}{\color{blue} %s}' % re.sub(r'(\.|\(|\))', r'\\\1', s_to)
         if 'X' in re_from:
             re_from = re_from.replace('X', '([\S]*)')
@@ -40,11 +40,11 @@ class AACheck(BasicCheck):
         self.aa_subs = load_aa_substitutes()
 
     def simple_check(self, document):
-        for line in document.text:
+        for iline, line in enumerate(document.text):
             for re_from,__, s_to, comment in self.aa_subs:
                 search = re.search(re_from, line, flags=re.I)
                 if search is not None:
-                    self.logger.warning(f'"{search[0]}" used, "{s_to}" recommended, {comment}')
+                    self.logger.warning(f'Line {iline}: "{search[0]}" used, "{s_to}" recommended, {comment}')
 
     def apply_aa_subs(self, line):
         for re_from, re_to, _, _ in self.aa_subs:
--- loader.py
+++ loader.py
@@ -10,11 +10,11 @@ import re
 
 class LaTeXloader(object):
     def __init__(self, filename):
-        self.splitter = re.compile(r'[ .,;:()*]', flags=re.IGNORECASE)
+        self.splitter = re.compile(r'[ ._,;:(){}*]', flags=re.IGNORECASE)
         self.words = set()
         self.text = open(filename, 'r').readlines()
         self.out_text = self.text
         for line in self.text:
             warr = self.splitter.split(line.strip().lower())
-            warr = [s for s in warr if len(s) > 3]
+            warr = [s for s in warr if len(s) > 3 and not re.fullmatch(r'[0-9\.]*', s)]
             self.words |= set(warr)
