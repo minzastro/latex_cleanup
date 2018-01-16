78404914dde1b8c4137dffdeebcca90c8f3e7358
Alexey Mints <mints@mps.mpg.de>
2018-01-16 21:54:36 +0100

    Config added

--- checks/basic.py
+++ checks/basic.py
@@ -6,6 +6,8 @@ Created on Thu Dec  7 12:00:57 2017
 @author: mints
 """
 import logging
+import os
+import json
 
 
 class BasicCheck(object):
@@ -18,7 +20,15 @@ class BasicCheck(object):
         handler.setFormatter(logging.Formatter(
             '%(name)s - %(levelname)s - %(message)s'))
         self.logger.addHandler(handler)
+        self.config = self.load_config()
 
+    def load_config(self):
+        conf_name = '%s.conf' % __file__[:-3]
+        if os.path.exists(conf_name):
+            return json.load(open(conf_name))
+        else:
+            return {}
+            
     def simple_check(self, document):
         raise NotImplemented
 
